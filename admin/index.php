a100b250dba37f48dd4057c08820c65521871f0f
Mark Mulli <markmulli001@gmail.com>
2024-01-20 17:59:30 +0300

    modified file

---
+++ admin/index.php
@@ -0,0 +1,34 @@
+<?php
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="admin_assets/admin_style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>DAMS</title>
+</head>
+<body>
+   <main>
+       <form action="" method="">
+           <h1>Doctor Appointment Management System</h1>
+           <span id="error"></span>
+           <div class="">
+               <input type="text" placeholder="Enter Email Address...">
+           </div>
+           <div class="">
+               <input type="password" placeholder="Password">
+           </div>
+           <div>
+               <button type="submit">Login</button>
+           </div>
+       </form>
+   </main>
+
+</body>
+</html>
+
