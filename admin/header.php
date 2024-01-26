bc6dd3d17b1dc25479eb02b8a2391f28f17b26be
Mark Mulli <markmulli001@gmail.com>
2024-01-26 15:05:16 +0300

    modified file

--- admin/header.php
+++ admin/header.php
@@ -6,7 +6,8 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="admin_assets/admin_style.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!--fonts-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
     <title>Administrator</title>
 </head>
 <body id="page-top">
@@ -14,12 +15,52 @@
     <div class="wrap">
         <ul class="sidebar">
             <a href="#" class="sidebar-brand">
-                <div class="icon-rotate">
-
-                </div>
-                <i  class="fa fa-laugh-wink"></i>
+<!--                <div class="icon-rotate">-->
+<!--                </div>-->
+                <i  class="fa-solid fa-face-laugh-wink"></i>
                 <div class="sidebar-text">Admin</div>
             </a>
+
+            <!--divider-->
+            <hr class="divider">
+
+            <!--Nav item dashboard-->
+            <li class="sidebar-item">
+                <a href="#" class="sidebar-link">
+                    <i class="fa fa-tachometer-alt"></i>
+                    <span>Dashboard</span>
+                </a>
+            </li>
+            <li class="sidebar-item">
+                <a href="#" class="sidebar-link">
+                    <i class="fa fa-user-md"></i>
+                    <span>Doctor</span>
+                </a>
+            </li>
+            <li class="sidebar-item">
+                <a href="#" class="sidebar-link">
+                    <i class="fa fa-procedures"></i>
+                    <span>Patient</span>
+                </a>
+            </li>
+            <li class="sidebar-item">
+                <a href="#" class="sidebar-link">
+                    <i class="fa fa-user-clock"></i>
+                    <span>Doctor Schedule</span>
+                </a>
+            </li>
+            <li class="sidebar-item">
+                <a href="#" class="sidebar-link">
+                    <i class="fa fa-notes-medical"></i>
+                    <span>Appointment</span>
+                </a>
+            </li>
+            <li class="sidebar-item">
+                <a href="#" class="sidebar-link">
+                    <i class="fa fa-id-card"></i>
+                    <span>Profile</span>
+                </a>
+            </li>
 
         </ul>
 
