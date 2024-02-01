aec3fab3cb94f22fdadfc840dfe2daa567c762b9
Mark Mulli <markmulli001@gmail.com>
2024-02-01 12:15:01 +0300

    dashboard section

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -6,6 +6,118 @@
     <!--page heading-->
     <h1 class="heading"> Dashboard </h1>
 
+    <div class="row">
+        <div class="column">
+            <div class="card border-left">
+                <div class="card-body">
+                    <div class="no-gutters">
+                        <div class="top-row">
+                            <div class="card-text">
+                                Today's Total Appointments
+                            </div>
+                            <div class="number">
+                                  0  <?php ?>
+                            </div>
+                        </div>
+                        <div class="bottom-row">
+                            <i class="fa fa-clipboard-list">
+
+                            </i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="column">
+            <div class="card border-left">
+                <div class="card-body">
+                    <div class="no-gutters">
+                        <div class="top-row">
+                            <div class="card-text">
+                                Yesterday's Total Appointments
+                            </div>
+                            <div class="number">
+                                0  <?php ?>
+                            </div>
+                        </div>
+                        <div class="bottom-row">
+                            <i class="fa fa-clipboard-list">
+
+                            </i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="column">
+            <div class="card border-left">
+                <div class="card-body">
+                    <div class="no-gutters">
+                        <div class="top-row">
+                            <div class="card-text">
+                                Last 7 Days Total Appointments
+                            </div>
+                            <div class="number">
+                                0  <?php ?>
+                            </div>
+                        </div>
+                        <div class="bottom-row">
+                            <i class="fa fa-clipboard-list">
+
+                            </i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="column">
+            <div class="card border-left">
+                <div class="card-body">
+                    <div class="no-gutters">
+                        <div class="top-row">
+                            <div class="card-text">
+                                Total Appointments till date
+                            </div>
+                            <div class="number">
+                                0  <?php ?>
+                            </div>
+                        </div>
+                        <div class="bottom-row">
+                            <i class="fa fa-clipboard-list">
+
+                            </i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="column">
+            <div class="card border-left">
+                <div class="card-body">
+                    <div class="no-gutters">
+                        <div class="top-row">
+                            <div class="card-text">
+                                Total Registered Patients
+                            </div>
+                            <div class="number">
+                                0  <?php ?>
+                            </div>
+                        </div>
+                        <div class="bottom-row">
+                            <i class="fa fa-clipboard-list">
+
+                            </i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
 
 
 
