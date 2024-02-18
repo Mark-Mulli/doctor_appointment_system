034876b2f1534f5b0ccca22f4f9ce4a11aabee26
Mark Mulli <markmulli001@gmail.com>
2024-02-18 15:15:11 +0300

    admin profile page

--- admin/admin_profile.php
+++ admin/admin_profile.php
@@ -1,5 +1,14 @@
 <?php
     include 'header.php';
+    include 'connectdb.php';
+
+
+    $query = "SELECT * FROM admin_table WHERE admin_id = ?";
+    $stmt = mysqli_prepare($conn,$query);
+    mysqli_stmt_bind_param($stmt, 'i', $_SESSION['admin_id']);
+    mysqli_stmt_execute($stmt);
+    $result = mysqli_stmt_get_result($stmt);
+
 ?>
 
 <!--page heading-->
@@ -26,32 +35,32 @@
                 <div class="card-body">
                     <div class="input-group">
                         <label>Admin Name</label>
-                        <input type="text" name="" id="" class="input">
+                        <input type="text" name="admin_name" id="admin_name" class="input">
                     </div>
                     <div class="input-group">
                         <label>Admin Email Address</label>
-                        <input type="text" name="" id="" class="input">
+                        <input type="text" name="admin_email_address" id="admin_email_address" class="input">
                     </div>
                     <div class="input-group">
                         <label>Password</label>
-                        <input type="text" name="" id="" class="input">
+                        <input type="text" name="admin_password" id="admin_password" class="input">
                     </div>
                     <div class="input-group">
                         <label>Hospital Name</label>
-                        <input type="text" name="" id="" class="input">
+                        <input type="text" name="hospital_name" id="hospital_name" class="input">
                     </div>
                     <div class="input-group">
                         <label>Hospital Address</label>
-                        <input type="text" name="" id="" class="input">
+                        <input type="text" name="hospital_address" id="hospital_address" class="input">
                     </div>
                     <div class="input-group">
                         <label>Hospital Contact No.</label>
-                        <input type="text" name="" id="" class="input">
+                        <input type="text" name="hospital_contact_no" id="hospital_contact_no" class="input">
                     </div>
                     <div class="input-group">
                         <label>Hospital logo</label>
                         <br>
-                        <input type="file" name="" id="">
+                        <input type="file" name="hospital_logo" id="hospital_logo">
                         <span id="uploaded_hospital_logo"></span>
                     </div>
                 </div>
@@ -60,6 +69,25 @@
     </div>
 
 </form>
+
+<script>
+    document.addEventListener("DOMContentLoaded", function() {
+        <?php foreach($result as $row): ?>
+        document.getElementById('admin_email_address').value = "<?php echo $row['admin_email_address']; ?>";
+        document.getElementById('admin_password').value = "<?php echo $row['admin_password']; ?>";
+        document.getElementById('admin_name').value = "<?php echo $row['admin_name']; ?>";
+        document.getElementById('hospital_name').value = "<?php echo $row['hospital_name']; ?>";
+        document.getElementById('hospital_address').value = "<?php echo $row['hospital_address']; ?>";
+        document.getElementById('hospital_contact_no').value = "<?php echo $row['hospital_contact_no']; ?>";
+
+        <?php if($row['hospital_logo'] != ''): ?>
+        document.getElementById("uploaded_hospital_logo").innerHTML = "<img src='<?php echo $row['hospital_logo']; ?>' class='img-thumbnail' width='100' /><input type='hidden' name='hidden_hospital_logo' value='<?php echo $row['hospital_logo']; ?>' />";
+        <?php else: ?>
+        document.getElementById("uploaded_hospital_logo").innerHTML = "<input type='hidden' name='hidden_hospital_logo' value='' />";
+        <?php endif; ?>
+        <?php endforeach; ?>
+    });
+</script>
 
 
 <?php
